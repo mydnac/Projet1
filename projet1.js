0955983fbf8fe058353c49c80f271d5b46aae748
Bryan Even <bryaneven@gmx.fr>
2020-03-10 15:42:23 +0100

    burge menu fini

--- projet1.js
+++ projet1.js
@@ -1,5 +1,15 @@
 let burger = document.getElementById('burger');
 let navbar = document.getElementById('navbar');
-burger.addEventListener ('click', (event) => { 
+let menumobile = document.getElementById('menumobile');
+// burger.addEventListener ('click', (event) => { 
+//     navbar.classList.toggle('open-burger');
+//     menumobile.classList.toggle('displaymenu');
+
+// })
+
+burger.addEventListener ('click', burgerDisplay)
+
+function burgerDisplay() {
     navbar.classList.toggle('open-burger');
-})
+    menumobile.classList.toggle('displayMenu');
+}
