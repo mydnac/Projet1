c65c9829d881a6758a3ad3455b6d5745e7d44253
Bryan Even <bryaneven@gmx.fr>
2020-03-12 17:13:52 +0100

    change color font menu

--- projet1.js
+++ projet1.js
@@ -1,12 +1,5 @@
 let burger = document.getElementById('burger');
 let navbar = document.getElementById('navbar');
-let menumobile = document.getElementById('menumobile');
-// burger.addEventListener ('click', (event) => { 
-//     navbar.classList.toggle('open-burger');
-//     menumobile.classList.toggle('displaymenu');
-
-// })
-
 burger.addEventListener ('click', burgerDisplay)
 
 function burgerDisplay() {
